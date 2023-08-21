06cd0a66625867d9404cb01e2d64d3d11e37d78a
NewSouthWave <141728350+NewSouthWave@users.noreply.github.com>
2023-08-21 21:18:18 +0900

    Add files via upload
    2023 8/21

--- bjoon_2438.cpp
+++ bjoon_2438.cpp
@@ -6,10 +6,15 @@ int main(void){
     scanf("%d", &n);
 
     for(int i = 1 ;i <= n; i++){
-        for(int j = 0; j < i; j++){
-            printf("*");
+        for(int j = n - i; j >= 1; j--){
+            printf(" ");
+        }
+
+        for(int k = 1; k <= i; k++){
+        printf("*");
         }
         printf("\n");
+
     }
 
 	return 0;
