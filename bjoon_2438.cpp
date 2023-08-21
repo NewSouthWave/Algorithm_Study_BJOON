506a815110d92fc170584d5ea9017f8a4e362be9
NewSouthWave <141728350+NewSouthWave@users.noreply.github.com>
2023-08-21 20:57:37 +0900

    Add files via upload
    2023 8/21

---
+++ bjoon_2438.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNNGS
+#include <stdio.h>
+
+int main(void){
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 1 ;i <= n; i++){
+        for(int j = 0; j < i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+	return 0;
+}
